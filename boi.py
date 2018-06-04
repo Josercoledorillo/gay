247c0c0e09f1de3bc423527cb867c6250fb072b9
Josercoledorillo <39567592+Josercoledorillo@users.noreply.github.com>
2018-06-04 14:29:27 -0400

    Update and rename boi to boi.py

--- boi.py
+++ boi.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 pygame.init()
 import time
 
@@ -29,19 +30,26 @@ time.sleep(4)
 
 first_level_first_question = str(input('How many cats can be put in an empty pool? A: none B: 1 C: 50'))
 if first_level_first_question == 'B':
-  print ('Correct! Once you put a cat in a pool, it will not be empty anymore.')
+  print ('Correct! Once you put a cat in a pool, it will not be empty anymore.')ns
+  Answered = Answered + 1
 else:
   print ('Incorrect! Once you put a cat in a pool, it will not be empty anymore.')
-  
+  Lives = Lives - 1
+  if Lives == 0:
+    print ('Game Over')
+    pygame.quit()
+
 first_level_second_question = str(input('What starts with e, ends with e, and has a letter inside? A: envelope B: eagle C: office'))
 if first_level_second_question == 'A':
   print ('Correct! first and last letter of envelope is e and they often contain letters.')
+  Answered = Answered + 1
 else:
   print ('Incorrect! first and last letter of envelope is e and they often contain letters.')
   
 first_level_third_question = str(input('What gets wet when you become dry? A: towel, B: fan, C: human'))
 if first_level_third_question == 'A':
   print ('Correct! towel soaks water, making you dry.')
+  Answered = Answered + 1
 else:
   print ('Incorrect! towel soaks water, making you dry.')
   
@@ -53,18 +61,21 @@ print ('Please continue to entertain me')
 second_level_first_question = str(input('I travel all around the world, but always stay in my corner, what am I? A: Canada B: Stamp C: mailman'))
 if second_level_first_question == 'B':
   print ('Correct! postal stamps go in the corner of envolepes.')
+  Answered = Answered + 1
 else:
   print ('Incorrect! postal stamps go in the corner of envolepes.')
   
 second_level_second_question = str(input('What disappears as soon as you say their name? A: Ghost B: Ivan C: Silence'))
 if second_level_second_question == 'C':
   print ('Correct! As soon as you talk, it is no longer silent')
+  Answered = Answered + 1
 else:
   print ('Incorrect! As soon as you talk, it is no longer silent')
 
 second_level_third_question = str(input('How many letters are in alphabet? A: 8 B: 11 C:26'))
 if second_level_third_question == 'A':
   print ('Correct! There are 8 letters in the word alphabet')
+  Answered = Answered + 1
 else:
   print ('Incorrect! There are 8 letters in the word alphabet')
 
@@ -75,23 +86,26 @@ print ('My best riddles are coming your way')
 third_level_first_question = str(input('What do you call a bear without an ear? A: Bear B: Disabled C: B'))
 if third_level_first_question == 'A':
   print ('Correct! A human without an ear is still refered to as a human')
+  Answered = Answered + 1
 else:
   print ('Incorrect! A human without an ear is still refered to as a human')
   
 third_level_second_question = str(input('What do you break before you use it? A: Ivan B: egg C: batteries'))
 if third_level_second_question == 'B':
   print ('Correct! You must discard the egg shell in order to use an egg.')
+  Answered = Answered + 1
 else:
   print ('Incorrect! You must discard the egg shell in order to use an egg.')
   
 third_level_third_question = str(input('This belongs to you, but it is used by everyone, what is it? A: Oxygen B: Name C: Assistance'))
 if third_level_third_question == 'B':
   print ('Correct! your name is yours, but people use it to call your attention')
+  Answered = Answered + 1
 else:
   print ('Incorrect! your name is yours, but people use it to call your attention')
   
 print ('Congratulations, you are the only mortal to have completed my riddles.')
-print (' Your final score is score')
+print (Score)
 
   
   
